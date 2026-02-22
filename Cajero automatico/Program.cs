91f3f5eaddd9255938c7ac385ea2ec3bdf097446
Julian Vazquez <juliandavivazquezsantana@gmail.com>
2026-02-22 14:24:24 -0400

    excepcion 2023-0715

--- Cajero automatico/Program.cs
+++ Cajero automatico/Program.cs
@@ -3,6 +3,20 @@ using System.Threading.Tasks;
 
 namespace ISW123_2023_0715
 {
+    public class SaldoInsuficienteException_0715 : Exception
+    {
+        public SaldoInsuficienteException_0715(string mensaje) : base(mensaje) { }
+    }
 
-    
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Julian Vazquez - Matrícula: 2023-0715");
+            Console.WriteLine("Sistema ATM Banco Popular - Iniciado");
+        }
+
+
+    }
+
 }
